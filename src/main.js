bdc3155e5c9dad5c3ffab5e16f9b4cc5720b4d1b
Bram <b93ram@gmail.com>
2023-09-10 17:10:22 -0400

    npm install and ran build

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router/index';
-import store from './store'
-// Disable production tip
+
 Vue.config.productionTip = false;
 
-// Create a new Vue instance with router configuration
+
 new Vue({
-  store,
   router,
   render: h => h(App)
 }).$mount('#app');
